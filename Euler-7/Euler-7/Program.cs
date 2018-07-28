a7dddefea43dc3977247da3c71a33125ae5d45d1
Segger87 <Segger87@gmail.com>
2018-07-28 19:55:56 +0100

    Euler-7 solution

---
+++ Euler-7/Euler-7/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Euler_7
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var p = new Program();
+			var listOfPrimes = p.PrimeNumbers();
+			p.PrintResult(listOfPrimes[10000]);
+		}
+
+		public List<int> PrimeNumbers()
+		{
+			var listOfPrimes = new List<int>();
+			for (int i = 0; i < 1000000; i++)
+			{
+				var prime = IsItAPrime(i);
+				if (prime)
+				{
+					listOfPrimes.Add(i);
+				}
+			}
+			return listOfPrimes;
+		}
+		public static bool IsItAPrime(int n)
+		{
+			if (n <= 1) return false;
+			if (n == 2) return true;
+			if (n % 2 == 0) return false;
+
+			var limit = (int)Math.Floor(Math.Sqrt(n));
+
+			for (int i = 3; i <= limit; i += 2)
+			{
+				if (n % i == 0) return false;
+			}
+			return true;
+		}
+
+		public void PrintResult(int result)
+		{
+			Console.WriteLine($"The 10,001 Prime number is {result}");
+			Console.ReadLine();
+		}
+	}
+}
+
